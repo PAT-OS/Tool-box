d634d68ee912f070c6f2ad16bf48c680ceaf521e
Patrick Pigmans <ppigmans@hotmail.com>
2016-07-27 15:32:00 +0200

    Update first-setup.sh

--- first-setup.sh
+++ first-setup.sh
@@ -13,15 +13,11 @@ make
 chmod +x set_overscan.sh
 mv set_overscan.sh /usr/bin/overscan
 cd ..
-cp -r ./set_overscan /user/bin/
+cp -r ./set_overscan /usr/bin/set_overscan
 chmod +x systeminfo
-chmod +x ipconfig
-mv systeminfo /usr/bin
-mv ipconfig /usr/bin
-
-echo "is dit een raspberry pi? ja of nee?"
-read x
-if [ "x$x" = "xja" ] ; then
+chmod +x ipconfig.sh
+mv systeminfo /usr/bin/systeminfo
+mv ipconfig.sh /usr/bin/ipconfig
 sudo curl -L --output /usr/bin/rpi-update https://raw.githubusercontent.com/Hexxeh/rpiupdate/master/rpi-update && sudo chmod +x /usr/bin/rpi-update
 sleep 4
 echo "we zijn klaar met het klaar zetten, zodra u deze tools wil gebruiken typ dan het volgende"
@@ -33,8 +29,24 @@ echo "set_overscan voor overscan"
 echo
 echo "rpi-update voor een update te draaien voor de raspberry pi"
 echo "Veel plezier!"
+exit 1
+fi
+
+
 else
-echo 
+echo "installatie zonder raspberry pi onderdelen begonnen"
+cd ./set_overscan
+apt-get install gcc automake -y
+make
+chmod +x set_overscan.sh
+mv set_overscan.sh /usr/bin/overscan
+cd ..
+cp -r ./set_overscan /usr/bin/set_overscan
+chmod +x systeminfo
+chmod +x ipconfig.sh
+mv systeminfo /usr/bin/systeminfo
+mv ipconfig.sh /usr/bin/ipconfig
+sleep 4
 echo "we zijn klaar met het klaar zetten, zodra u deze tools wil gebruiken typ dan het volgende"
 echo "systeminfo voor system info"
 echo
@@ -42,10 +54,7 @@ echo "ipconfig voor ipconfig"
 echo
 echo "set_overscan voor overscan"
 echo
-echo "Veel Plezier!"
-fi
-
-if [ "x$x" = "xnee" ] ; then
-echo "installatie geannuleert"
+echo "Veel plezier!"
+exit 1
 fi
 
